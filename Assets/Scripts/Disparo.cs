a575035e9b8613e37e4adf31a056876f6e2df5e8
Isaac Cisneros <55520605+ZPSMX@users.noreply.github.com>
2023-09-18 11:48:01 -0700

    0.2.0
    *animacion de pistola al estar quieta y al estar accionando el disparo
    
    *programacion de animacion disparo

--- Assets/Scripts/Disparo.cs
+++ Assets/Scripts/Disparo.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Disparo : MonoBehaviour
 {
     [SerializeField] private Transform controladorDisparo;
     [SerializeField] private GameObject bala;
+    public Animator animator;
+    private bool disparoActivo;
 
-    private void Disparar()
+    public void DispararSi()
     {
-        Instantiate(bala, controladorDisparo.position, controladorDisparo.rotation);
+        disparoActivo = true;
+        if (disparoActivo == true)
+        {
+            Instantiate(bala, controladorDisparo.position, controladorDisparo.rotation);
+            animator.SetBool("Disparar", true);
+            
+           
+        }
+    }
+
+    public void DispararNo()
+    {
+        disparoActivo = false;
+        if (disparoActivo == false)
+        {
+            animator.SetBool("Disparar", false);
+           
+        }
     }
 }
 
